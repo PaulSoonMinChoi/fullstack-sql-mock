050ec7ca19117cc3367b65c6cbe083b206142b36
Paul Choi <paulsoonminchoi@gmail.com>
2020-09-20 12:11:29 -0700

    completed all of backend and tested all routes/controllers/dbhelpers

--- db/dbhelpers.js
+++ db/dbhelpers.js
@@ -1,11 +1,52 @@
 // complete and fix the dbhelpers
+const db = require('../db');
+
+const helpers = {
+  getProductsHelper: (callback) => {
+    let queryStr = `SELECT * FROM entry`
+    db.query(queryStr, (err, results) => {
+      if (err) {
+        callback(err);
+      } else {
+        callback(null, results);
+      }
+    })
+  },
+
+  postProductsHelper: (data, callback) => {
+    let queryStr = `INSERT INTO entry(item, min_cost, curr_bid, ends_in, image) VALUES('${data.item}',${data.min_cost},${data.curr_bid},${data.ends_in},'${data.image}')`
+    db.query(queryStr, (err, results) => {
+      if (err) {
+        callback(err);
+      } else {
+        callback(null, results);
+      }
+    })
+  },
+
+  updateProductHelper: (id, data, callback) => {
+    let queryStr = `UPDATE entry SET item = '${data.item}', min_cost = ${data.min_cost}, curr_bid = ${data.curr_bid}, ends_in = ${data.ends_in}, image = '${data.image}' WHERE id = ${id}`
+    db.query(queryStr, (err, results) => {
+      if (err) {
+        callback(err);
+      } else {
+        callback(null, results);
+      }
+    })
+  },
+
+  deleteProductHelper: (id, callback) => {
+    let queryStr = `DELETE FROM entry WHERE id = ${id}`
+    db.query(queryStr, (err, results) => {
+      if (err) {
+        callback(err);
+      } else {
+        callback(null, results);
+      }
+    })
+  }
+}
 
 
-getProductsHelper = () => 
 
-postProductsHelper = () => 
-
-updateProductHelper = () => 
-
-deleteProductHelper = () => 
-
+module.exports = helpers;
